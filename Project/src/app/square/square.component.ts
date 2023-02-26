7547f61f6c6aa71cfa587be7153b4d4d65410935
walid885 <bettaieb88walid@gmail.com>
2023-02-26 18:08:20 +0100

    generating the Board component

--- Project/src/app/square/square.component.ts
+++ Project/src/app/square/square.component.ts
@@ -1,15 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   template: `
-    <p>
-      square works!
-    </p>
+  <button>{{ Value }}</button>
   `,
   styles: [
   ]
 })
 export class SquareComponent {
-
+@Input() Value: 'X' | 'O' = 'X'; 
 }
