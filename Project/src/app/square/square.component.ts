bed29193e101bdd91b2af06a180a4a775c7040c9
walid885 <bettaieb88walid@gmail.com>
2023-02-26 17:44:59 +0100

    using the first component

---
+++ Project/src/app/square/square.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <p>
+      square works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SquareComponent {
+
+}
