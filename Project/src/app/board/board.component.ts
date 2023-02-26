6b1b9789dc571536e4c91e0b7531043a50b35dc3
walid885 <bettaieb88walid@gmail.com>
2023-02-26 18:46:42 +0100

    there is a dependency problem to be resolved , but due to time constraint , I will postpone it to later date , dependency problem -> downgrading from Angular 15 -> angular 9

--- Project/src/app/board/board.component.ts
+++ Project/src/app/board/board.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent {
+export class BoardComponent implements OnInit {
+squares : any[]; 
+xIsNext: boolean ; 
+winner: string; 
 
+constructor(){}
+ngOnInit(): void {
+    this.newGame();
+}
+newGame(){
+  this.squares= Array(9).fill(null); 
+  this.xIsNext = true ; 
+  this.winner = "null" ;
+
+}
 }
